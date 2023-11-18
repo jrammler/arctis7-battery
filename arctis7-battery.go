1d532e5e805c8729a4679032956c0983784e7278
Johannes Rammler <johannes.rammler@gmail.com>
2023-11-18 10:18:59 +0100

    Fix crash when headset is not connected

--- arctis7-battery.go
+++ arctis7-battery.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -9,30 +10,30 @@ import (
 	"github.com/sstallion/go-hid"
 )
 
-func getBattery(d *hid.Device) int {
+func getBattery(d *hid.Device) (int, error) {
 	_, err := d.Write([]byte{0x06, 0x14})
 	if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
 	report := make([]byte, 31)
 	_, err = d.Read(report)
 	if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
 	if report[2] != 0x03 {
-		log.Fatal("Headset not connected")
+		return 0, errors.New("Headset not connected")
 	}
 
 	_, err = d.Write([]byte{0x06, 0x18})
 	if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
 	_, err = d.Read(report)
 	if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
 
-	return int(report[2])
+	return int(report[2]), nil
 }
 
 func main() {
@@ -40,28 +41,39 @@ func main() {
 	flag.Parse()
 
 	if err := hid.Init(); err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error when initializing HID library: %v", err)
 	}
 
 	// Open the device using the VID and PID.
 	d, err := hid.OpenFirst(0x1038, 0x12ad)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Unable to connect to headset receiver. Is it connected?")
 	}
 
 	defer func() {
 		err = d.Close()
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalf("Error when closing HID library: %v", err)
 		}
 	}()
 
 	if *delay > 0 {
 		for true {
-			fmt.Printf("%d\n", getBattery(d))
+			value, err := getBattery(d)
+			if err != nil {
+				fmt.Print("--\n");
+			} else {
+				fmt.Printf("%d\n", value)
+			}
 			time.Sleep(time.Duration(*delay) * time.Second)
 		}
 	} else {
-		fmt.Printf("%d\n", getBattery(d))
+		value, err := getBattery(d)
+		if err != nil {
+			fmt.Print("--\n")
+			log.Print(err)
+		} else {
+			fmt.Printf("%d\n", value)
+		}
 	}
 }
